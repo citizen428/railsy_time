62e6e0f0effcad886853127e84188f193f3c3996
Michael Kohl <me@citizen428.net>
2023-05-21 10:48:56 +0700

    Initialize package

---
+++ example/railsy_time_example.dart
@@ -0,0 +1,6 @@
+import 'package:railsy_time/railsy_time.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/railsy_time_test.dart
@@ -0,0 +1,16 @@
+import 'package:railsy_time/railsy_time.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
